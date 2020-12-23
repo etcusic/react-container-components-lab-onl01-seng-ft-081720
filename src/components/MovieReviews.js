71ce5d5159841f8aa65e11d97e6cb7cf38cc69ab
Eli Cusic <tntitans31@gmail.com>
2020-12-23 11:45:31 -0600

    commit tree 96c87a2b4de2a92718fb9cd7fbbfa90b88cb935c

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = () => {
+    return (
+        <div className="review-list">
+            <div className="review"></div>
+        </div>
+    )
+}
+
+export default MovieReviews
